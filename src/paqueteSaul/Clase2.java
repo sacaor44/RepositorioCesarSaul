062abd2ce2ae5a6cdd4b1a98b7ab8be286a8fc0f
Diurno <Diurno@14-C7>
2013-05-03 12:09:17 +0200

    Clase 2 realizada

---
+++ src/paqueteSaul/Clase2.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteSaul;
+
+/**
+ *
+ * @author Diurno
+ */
+public class Clase2 {
+    private int x;
+
+    public Clase2() {
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+    
+    
+}
